a57791191932aaeec5be02937b4c9f9c1b3495f3
Ruan <droderuan@hotmail.com>
2019-10-15 02:48:30 -0300

    changed wsgi

--- pontos_turisticos/wsgi.py
+++ pontos_turisticos/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
+#linha para resolver um problema de push no heroku
 
 import os
 
@@ -14,4 +15,5 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontos_turisticos.settings')
 
+application = get_wsgi_application()
 application = Cling(get_wsgi_application())
